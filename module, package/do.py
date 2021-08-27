1d8f774aa920b952bc26c15d5e4eee0de4dbc4b1
따삐 <w2005@e-mirimi.hs.kr>
2021-08-27 10:18:37 +0900

    package

--- module, package/do.py
+++ module, package/do.py
@@ -1,4 +1,25 @@
 import my_module as mm
-
 mm.지원이()
 print(mm.date)
+
+import travel.gangreung as ga
+gp = ga.GangreungPackage()
+print(gp)
+
+
+from travel import gangreung
+gp = gangreung.GangreungPackage()
+print(gp)
+
+from travel.gangreung import GangreungPackage
+gp = GangreungPackage()
+print(gp)
+
+from travel import ilsan
+ilsan.이지()
+
+from travel import*
+ilsan.이지()
+
+gp=gangreung.GangreungPackage()
+print(gp)
