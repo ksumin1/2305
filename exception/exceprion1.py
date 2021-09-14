4415f54f72947fc7f22c7e2f69a5f77b4e9ed507
따삐 <w2005@e-mirimi.hs.kr>
2021-09-14 12:16:45 +0900

    python 예외처리 문법

---
+++ exception/exceprion1.py
@@ -0,0 +1,24 @@
+'''
+
+java
+try{
+
+}catch(FileNotFoundException){
+
+}finally{
+//무조건 실행}
+
+python
+try:
+    # 예외가 의심되는 코드
+except ValueError as e:
+    # ValueError 처리하는 코드
+except:
+    # 예외가 났을 때 처리하느 코드
+else:
+    # 예외가 발생하지 않을때 실행하는 코드
+finally:
+    # 무조건 실행해야하는 코드
+'''
+
+
